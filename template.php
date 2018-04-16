f8a56ac1364ef884bca1c7109c60a5f9633833b1
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-04-16 16:19:21 +0100

    updated the template to hopefully add img-resposnive to all images

--- template.php
+++ template.php
@@ -4,21 +4,19 @@
  * The primary PHP file for this theme.
  */
 
- 
- 
-function aesbs337_preprocess_page(&$vars) { 
-    // template files called page--contenttype.tpl.php 
-    if (isset($vars['node']->type)) { 
-        $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type; 
+function aesbs337_preprocess_page(&$vars) {
+    // template files called page--contenttype.tpl.php
+    if (isset($vars['node']->type)) {
+        $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
     }
-	
+
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/logoscroll.js');
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/fade-text.js');
 }
 
 function aesbs337_theme(){
 	$items=array();
-	
+
 	$items['user_login']=array(
 	'render element' => 'form',
 	'path'=> drupal_get_path('theme', 'aesbs337').'/templates',
@@ -28,4 +26,8 @@ function aesbs337_theme(){
 	),
 	);
 	return $items;
+}
+
+function aesbs337_preprocess_image_style(&$vars) {
+	$vars['attributes']['class'][] = 'img-responsive';
 }
