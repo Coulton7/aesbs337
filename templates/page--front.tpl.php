4ad1798cac63bcfc59c7579e685eecb67cf4e882
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-04-25 12:50:04 +0100

    Editing the front page template to stop calling a block

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -159,10 +159,6 @@
 		<?php if ($page['preface']): ?>
 			<div class="preface">
 				<?php print render($page['preface']);?>
-							<?php
-								$block = module_invoke('fieldblock' , 'block_view' , 'c95f1325a4ef64f12582e4a941d72fda');
-								print render ($block ['content']);
-							?>
 			</div>
 
 			<div class="row wavelower row-eq-height">
