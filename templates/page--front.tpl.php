abbe862cf181a8565e7930f088fcafc7b9455176
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-01 11:51:21 +0100

    matched the theme across the site

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -140,21 +140,27 @@
 								<?php if (!empty($page['navigation'])): ?>
 									<?php print render($page['navigation']); ?>
 								<?php endif; ?>
+								<div class="section-shadow-menu"></div>
 							</nav>
 						</div>
 					</div>
 				<?php endif; ?>
 
-				<?php if ($page['navigation_col']): ?>
-					<div class="navigation_col">
-						<div class="action-menu text-center">
-							<?php print render($page['navigation_col']);?>
-						</div>
-					</div>
-				<?php endif; ?><!--added by Matt -->
 			</header>
 		</div>
 	</div>
+
+	<?php if ($page['navigation_col']): ?>
+		<div class="navigation_col">
+			<div class="action-menu text-center">
+				<div class="col-sm-2 col-xs-2 fullscreen">
+				</div>
+				<div class="col-sm-10 col-xs-10 fullscreen">
+				<?php print render($page['navigation_col']);?>
+			</div>
+			</div>
+		</div>
+	<?php endif; ?><!--added by Matt -->
 
 	<?php if ($page['preface']): ?>
 	<div class="preface front__flex-margin">
