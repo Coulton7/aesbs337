5db9052b2188409f113e7ca0eed2ff9ed95aee9c
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-02 15:34:59 +0100

    print render was incorrect with : instead of ;

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -186,7 +186,7 @@
 
 			<img class="curve-down" src = "/sites/all/themes/aesbs337/images/logos/wave-lower.svg" img alt ="curve-down"></img>
 
-			<?php print render($page['wavelower']): ?>
+			<?php print render($page['wavelower']);?>
 					<div class="socialmedia text-center">
 					<?php
 						$block = module_invoke('views', 'block_view', 'social_icons-block');
