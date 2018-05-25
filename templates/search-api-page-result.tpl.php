29ba774b33f4cb24bf88a5b1f562348b6da3bd69
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-25 15:02:37 +0100

    Re-added dotted border to the template

--- templates/search-api-page-result.tpl.php
+++ templates/search-api-page-result.tpl.php
@@ -65,12 +65,12 @@
  */
 ?>
 <li class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div class="dotted-border">
     <?php print render($title_prefix); ?>
     <h3 class="title"<?php print $title_attributes; ?>>
       <a href="<?php print $url; ?>"><?php print $title; ?></a>
     </h3>
     <?php print render($title_suffix); ?>
-    <?php print $result['node']->type; ?>
       <div class="search-snippet-info">
         <?php if ($snippet): ?>
           <p class="search-snippet"<?php print $content_attributes; ?>>
@@ -78,5 +78,6 @@
             <a href="<?php print $url; ?>">Read more</a>
           <?php endif; ?>
         </div>
+    </div>
 </li>
 <br>
