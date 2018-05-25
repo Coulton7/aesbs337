50de895b88c0f147cd265fed6ee5cb744eb72227
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-25 14:46:27 +0100

    Removed a non useful class

--- templates/search-api-page-result.tpl.php
+++ templates/search-api-page-result.tpl.php
@@ -66,19 +66,18 @@
 ?>
 <li class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="dotted-border">
-  <?php print render($title_prefix); ?>
-  <h3 class="title"<?php print $title_attributes; ?>>
-    <a href="<?php print $url; ?>"><?php print $title; ?></a>
-  </h3>
-  <?php print render($title_suffix); ?>
-  <?php print node_type_get_name($type); ?>
-  <div class="search-snippet-info">
-    <?php if ($snippet): ?>
-      <p class="search-snippet"<?php print $content_attributes; ?>>
-        <?php print (filter_xss($snippet, $allowed_tags = array(''))); ?></p>
-        <a href="<?php print $url; ?>">Read more</a>
-    <?php endif; ?>
-  </div>
-  </div>
+    <?php print render($title_prefix); ?>
+    <h3 class="title"<?php print $title_attributes; ?>>
+      <a href="<?php print $url; ?>"><?php print $title; ?></a>
+    </h3>
+    <?php print render($title_suffix); ?>
+      <div class="search-snippet-info">
+        <?php if ($snippet): ?>
+          <p class="search-snippet"<?php print $content_attributes; ?>>
+            <?php print (filter_xss($snippet, $allowed_tags = array(''))); ?></p>
+            <a href="<?php print $url; ?>">Read more</a>
+          <?php endif; ?>
+        </div>
+    </div>
 </li>
 <br>
