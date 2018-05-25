3eb3412eb77f7b5731d3dbddcb8f33b17e569c41
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-25 15:14:09 +0100

    Back to normal not template that is causing the issue

--- templates/search-api-page-result.tpl.php
+++ templates/search-api-page-result.tpl.php
@@ -65,19 +65,19 @@
  */
 ?>
 <li class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class ="dotted-border">
-  <?php print render($title_prefix); ?>
-  <h3 class="title"<?php print $title_attributes; ?>>
-    <a href="<?php print $url; ?>"><?php print $title; ?></a>
-  </h3>
-  <?php print render($title_suffix); ?>
-  <div class="search-snippet-info">
-    <?php if ($snippet): ?>
-      <p class="search-snippet"<?php print $content_attributes; ?>><?php print $snippet; ?></p>
-    <?php endif; ?>
-    <?php if ($info): ?>
-      <p class="search-info"><?php print $info; ?></p>
-    <?php endif; ?>
-  </div>
-</div>
+  <div class="dotted-border">
+    <?php print render($title_prefix); ?>
+    <h3 class="title"<?php print $title_attributes; ?>>
+      <a href="<?php print $url; ?>"><?php print $title; ?></a>
+    </h3>
+    <?php print render($title_suffix); ?>
+      <div class="search-snippet-info">
+        <?php if ($snippet): ?>
+          <p class="search-snippet"<?php print $content_attributes; ?>>
+            <?php print (filter_xss($snippet, $allowed_tags = array(''))); ?></p>
+            <a href="<?php print $url; ?>">Read more</a>
+          <?php endif; ?>
+        </div>
+    </div>
 </li>
+<br>
