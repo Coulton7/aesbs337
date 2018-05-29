fec876bd0f475c7309f086f52b12ded696ce6f9e
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-29 13:56:21 +0100

    Trying read more in quotation marks in stead of $title

--- templates/search-api-page-result.tpl.php
+++ templates/search-api-page-result.tpl.php
@@ -50,7 +50,7 @@
 				<?php if ($snippet): ?>
 				<p class="search-snippet"<?php print $content_attributes; ?>>
 				<?php print (filter_xss($snippet, $allowed_tags = array(''))); ?></p>
-				<a href="<?php print $url l($title, $url['path'], $url['options']); ?>">Read more</a>
+				<?php print $url ? l('Read more', $url['path'], $url['options']) : check_plain($title); ?>
 			<?php endif; ?>
 		</div>
 	</div>
