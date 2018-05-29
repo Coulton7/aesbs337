6c20ecd22e5a1182156c2d5445f4cc6e913a6205
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-29 15:20:00 +0100

    Adding a header tags around the  command

--- templates/search-api-page-result.tpl.php
+++ templates/search-api-page-result.tpl.php
@@ -46,9 +46,7 @@
 		<h3 class="title">
 		<?php print $url ? l($title, $url['path'], $url['options']) : check_plain($title); ?>
 		</h3>
-    <h6 class="search-contenttype">
-      <?php print $entity->type;?>
-    </h6>
+    <h6><?php print $entity->type;?></h6>
 		<div class="search-snippet-info">
 				<?php if ($snippet): ?>
 				<p class="search-snippet"<?php print $content_attributes; ?>>
