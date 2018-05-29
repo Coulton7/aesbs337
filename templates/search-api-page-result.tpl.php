ad82009ead387585942edeed2007517722f1d97d
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-29 12:53:21 +0100

    Adding a br on the bottom and the new php file style (mainly for links)

--- templates/search-api-page-result.tpl.php
+++ templates/search-api-page-result.tpl.php
@@ -1,82 +1,58 @@
 <?php
-
 /**
  * @file
  * Default theme implementation for displaying a single search result.
  *
  * This template renders a single search result and is collected into
- * search-results.tpl.php. This and the parent template are
- * dependent to one another sharing the markup for definition lists.
+ * search-api-page-results.tpl.php. This and the parent template are dependent
+ * on one another, sharing the markup for definition lists.
+ *
+ * View mode is set in the Search page settings. If you select
+ * "Themed as search results", then this template will be used for theming the
+ * individual results. Any other view mode will bypass this template.
  *
  * Available variables:
+ * - $index: The search index this search is based on.
  * - $url: URL of the result.
- * - $title: Title of the result.
- * - $snippet: A small preview of the result. Does not apply to user searches.
- * - $info: String of all the meta information ready for print. Does not apply
- *   to user searches.
+ * - $title: Title of the result (unsanitized).
+ * - $snippet: A small preview of the result.
+ * - $info: String of all the meta information ready for print. Applies
+ *   only if the result is a node.
  * - $info_split: Contains same data as $info, split into a keyed array.
- * - $module: The machine-readable name of the module (tab) being searched, such
- *   as "node" or "user".
- * - $title_prefix (array): An array containing additional output populated by
- *   modules, intended to be displayed in front of the main title tag that
- *   appears in the template.
- * - $title_suffix (array): An array containing additional output populated by
- *   modules, intended to be displayed after the main title tag that appears in
- *   the template.
+ * - $classes: CSS classes for this list element.
  *
  * Default keys within $info_split:
- * - $info_split['module']: The module that implemented the search query.
- * - $info_split['user']: Author of the node linked to users profile. Depends
- *   on permission.
- * - $info_split['date']: Last update of the node. Short formatted.
- * - $info_split['comment']: Number of comments output as "% comments", %
- *   being the count. Depends on comment.module.
- *
- * Other variables:
- * - $classes_array: Array of HTML class attribute values. It is flattened
- *   into a string within the variable $classes.
- * - $title_attributes_array: Array of HTML attributes for the title. It is
- *   flattened into a string within the variable $title_attributes.
- * - $content_attributes_array: Array of HTML attributes for the content. It is
- *   flattened into a string within the variable $content_attributes.
+ * - $info_split['user']: Author of the entity, where an author exists.
+ *   Depends on permission.
+ * - $info_split['date']: Last update of the entity, if the 'updated'
+ *   field exists. Short formatted.
  *
  * Since $info_split is keyed, a direct print of the item is possible.
- * This array does not apply to user searches so it is recommended to check
- * for its existence before printing. The default keys of 'type', 'user' and
- * 'date' always exist for node searches. Modules may provide other data.
- * @code
- *   <?php if (isset($info_split['comment'])): ?>
- *     <span class="info-comment">
- *       <?php print $info_split['comment']; ?>
- *     </span>
- *   <?php endif; ?>
- * @endcode
+ * This array applies where the search result is a node, so it is
+ * recommended to check for its existence before printing.
+ * Where the result is a node, the default keys of 'user' and 'date'
+ * will always exist.
  *
  * To check for all available data within $info_split, use the code below.
  * @code
  *   <?php print '<pre>'. check_plain(print_r($info_split, 1)) .'</pre>'; ?>
  * @endcode
  *
- * @see template_preprocess()
- * @see template_preprocess_search_result()
- * @see template_process()
- *
- * @ingroup themeable
+ * @see template_preprocess_search_api_page_result()
  */
 ?>
-<li class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class="dotted-border">
-    <?php print render($title_prefix); ?>
-    <h3 class="title"<?php print $title_attributes; ?>>
-      <a href="<?php print $url; ?>"><?php print $title; ?></a>
-    </h3>
-    <?php print render($title_suffix); ?>
-      <div class="search-snippet-info">
-        <?php if ($snippet): ?>
-          <p class="search-snippet"<?php print $content_attributes; ?>>
-            <?php print (filter_xss($snippet, $allowed_tags = array(''))); ?></p>
-          <?php endif; ?>
-        </div>
-    </div>
+<li class="search-result">
+	<div class="dotted-border">
+		<h3 class="title">
+		<?php print $url ? l($title, $url['path'], $url['options']) : check_plain($title); ?>
+		</h3>
+		<div class="search-snippet-info">
+				<?php if ($snippet): ?>
+				<p class="search-snippet"<?php print $content_attributes; ?>>
+				<?php print (filter_xss($snippet, $allowed_tags = array(''))); ?></p>
+				<a href="<?php print $url; ?>">Read more</a>
+			<?php endif; ?>
+		</div>
+	</div>
 </li>
 <br>
