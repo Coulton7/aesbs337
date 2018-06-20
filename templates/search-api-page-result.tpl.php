3d7e6c767bd62da69a18871c4483004c3ab7c083
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-06-20 12:01:53 +0100

    Converting back to what people say prints content type

--- templates/search-api-page-result.tpl.php
+++ templates/search-api-page-result.tpl.php
@@ -47,7 +47,7 @@
 		<?php print $url ? l($title, $url['path'], $url['options']) : check_plain($title); ?>
 		</h3>
     <h6>
-      <?php print $result['node']->field_types; ?>
+      <?php print $result['node']->type; ?>
     </h6>
 		<div class="search-snippet-info">
 				<?php if ($snippet): ?>
