3d85e0c94697961495b6a143b04d3062582dec85
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-08-14 16:38:08 +0100

    Edited page.tpl footer to be one tag and to have two divs located inside

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -326,16 +326,18 @@
 			<?php endif; ?>
 </div>
 
-<?php if (!empty($page['footer'])): ?>
-  <footer class="footer <?php print $container_class; ?> footer-background">
+<footer>
+	<?php if (!empty($page['footer'])): ?>
+  	<div class="footer <?php print $container_class; ?> footer-background">
 	<?php print render($page['footer']); ?>
-  </footer>
-<?php endif; ?>
+		</div>
+	<?php endif; ?>
 
-<?php if ($page['footer_lower']): ?>
-				<footer class="footer_lower <?php print $container_class; ?> dark-grey-gradient">
-					<div class="section-shadow">
-						<?php print render($page['footer_lower']);?>
-					</div>
-				</footer>
+	<?php if ($page['footer_lower']): ?>
+			<div class="footer_lower <?php print $container_class; ?> dark-grey-gradient">
+				<div class="section-shadow">
+					<?php print render($page['footer_lower']);?>
+				</div>
+			</div>
+	</footer>
 			<?php endif; ?><!--added by Matt -->
