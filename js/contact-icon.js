cadc4a8aafc4ccb12243adbbc944778e5eb152e5
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-02-20 12:15:34 +0000

    Updated contact-icon.js to have a limit
    Updated the contact-icon js that the icon will only change 15 times then stop and just show the phone icon constantly

--- js/contact-icon.js
+++ js/contact-icon.js
@@ -1,23 +1,28 @@
-(function($){
+(function($) {
 
-  $.fn.contactIcon = function(){
+  $.fn.contactIcon = function() {
 
     var contact_icon = jQuery(".contact_icon");
     var contactIcon = -1;
+    var runCount = 0;
 
     function showNextIcon() {
+      if (runCount < 15) {
         ++contactIcon;
         contact_icon.eq(contactIcon % contact_icon.length)
-            .show(0)
-            .delay(5500)
-            .hide(0, showNextIcon);
+          .show(0)
+          .delay(5500)
+          .hide(0, showNextIcon);
+        runCount++;
+      } else {
+        contact_icon.eq(contactIcon % contact_icon.length)
+          .show(0)
+      }
     }
-
     showNextIcon();
-  }
-
+  };
 })(jQuery);
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function() {
   jQuery('.socialmedia').contactIcon();
 });
