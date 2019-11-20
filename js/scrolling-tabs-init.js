4f2aa81b2a3bc5e4a70c7617d8cc3b5cfca2b570
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-11-20 12:44:06 +0000

    Apprently script was missing from file so here is the actual script

---
+++ js/scrolling-tabs-init.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+  $(activate);
+  function activate(){
+    $('.nav-tabs').scorllingTabs({
+      enableSwiping: true
+    }).on('ready.scrtabs', function(){
+      $('.tab-content').show();
+    });
+  }
+}());
