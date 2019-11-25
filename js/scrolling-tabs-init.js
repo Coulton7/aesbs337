4871868113777216c246a62ed6dc589e143b31a4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-11-25 16:03:02 +0000

    new arrows for the tabs

--- js/scrolling-tabs-init.js
+++ js/scrolling-tabs-init.js
@@ -5,7 +5,13 @@
 
     function activate() {
       $('.nav-tabs').scrollingTabs({
-        enableSwiping: true
+        enableSwiping: true,
+        rightArrowContent: [
+          '<i class="fas fa-caret-right fa-lg right-arrow"></i>'
+        ].join(''),
+        leftArrowContent: [
+          '<i class="fas fa-caret-left fa-lg left-arrow"></i>'
+        ].join('')
       }).on('ready.scrtabs', function() {
         $('.tab-content').show();
       });
