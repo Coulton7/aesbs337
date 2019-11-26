8a74b9b9d5e438057d721221173b72f981e66e17
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-11-26 10:40:33 +0000

    Changing the classes further to be correct content

--- js/scrolling-tabs-init.js
+++ js/scrolling-tabs-init.js
@@ -6,8 +6,8 @@
     function activate() {
       $('.nav-tabs').scrollingTabs({
         enableSwiping: true,
-        cssClassLeftArrow: '<i class="fas fa-caret-left fa-lg left-arrow"></i>',
-        cssClassRightArrow: '<i class="fas fa-caret-right fa-lg right-arrow"></i>'
+        cssClassLeftArrow: 'fas fa-caret-left fa-2x left-arrow',
+        cssClassRightArrow: 'fas fa-caret-right fa-2x right-arrow'
       }).on('ready.scrtabs', function() {
         $('.tab-content').show();
       });
