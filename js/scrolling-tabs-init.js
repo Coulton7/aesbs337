4ccd1752d383f61bb84b75efbdadf48f4adf0b9b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-11-27 14:55:27 +0000

    Adding a class to the tabs to make them thicher and more mobile friendly

--- js/scrolling-tabs-init.js
+++ js/scrolling-tabs-init.js
@@ -6,8 +6,8 @@
     function activate() {
       $('.nav-tabs').scrollingTabs({
         enableSwiping: true,
-        cssClassLeftArrow: 'fas fa-caret-left fa-3x left-arrow',
-        cssClassRightArrow: 'fas fa-caret-right fa-3x right-arrow'
+        cssClassLeftArrow: 'fas fa-caret-left fa-3x padding-1em left-arrow',
+        cssClassRightArrow: 'fas fa-caret-right fa-3x padding-1em right-arrow'
       }).on('ready.scrtabs', function() {
         $('.tab-content').show();
       });
