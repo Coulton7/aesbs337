db56fc6d8df618912ede3142fb5e14046ace7ea7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-01-17 11:14:39 +0000

    Adding a new variable to the scorlling tabds script
    Added a new variable to shrink the size of the scrolling tabs container (without affecting current css)so the arrow buttons can get the increased padding required to be more controllabale on mobile

--- js/scrolling-tabs-init.js
+++ js/scrolling-tabs-init.js
@@ -6,6 +6,7 @@
     function activate() {
       $('.nav-tabs').scrollingTabs({
         enableSwiping: true,
+        widthMultiplier: 0.95,
         cssClassLeftArrow: 'fas fa-caret-left fa-3x left-arrow',
         cssClassRightArrow: 'fas fa-caret-right fa-3x right-arrow'
       }).on('ready.scrtabs', function() {
