81689e470ee911f7cad0afb70bbfe8149ec598b4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-01-31 12:20:32 +0000

    Forogt the to make script look for id with # missing

--- js/email-print.js
+++ js/email-print.js
@@ -4,7 +4,7 @@
       document.location.href = "'mailto:?subject=' + encodeURIComponent(document.title) + '&body=' + encodeURIComponent(document.URL));return false;";
     });
   });
-  $("print-btn").click(function(){
+  $("#print-btn").click(function(){
     window.print();
   }
 );
