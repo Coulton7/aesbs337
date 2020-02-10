62c729fd6bb00e3f5216a2da9fa9cc33b7d9dd9b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-10 14:19:22 +0000

    $ not a function fix for jQuery should work

--- js/maintenance-counter.js
+++ js/maintenance-counter.js
@@ -1,19 +1,23 @@
-function countdown(){
-  var i = document.getElementById('counter');
-  if (parseInt(i.innerHTML)<=0){
+(function($) {
+  function countdown() {
+    var i = document.getElementById('counter');
+    if (parseInt(i.innerHTML) <= 0) {
 
-  document.getElementById('replace').textContent= "If re-direct has failed click the link to the global site";
-  $('#ext-link').removeClass("display-none");
-  return;
+      document.getElementById('replace').textContent = "If re-direct has failed click the link to the global site";
+      $('#ext-link').removeClass("display-none");
+      return;
 
+    }
+    if (parseInt(i.innerHTML) != 0) {
+      i.innerHTML = parseInt(i.innerHTML) - 1;
+    }
   }
-  if (parseInt(i.innerHTML)!=0){
-    i.innerHTML = parseInt(i.innerHTML)-1;
-  }
-}
+})(jQuery);
 
-(function($){
-  $(document).ready(function(){
-    setInterval(function(){countdown();},1000);
+(function($) {
+  $(document).ready(function() {
+    setInterval(function() {
+      countdown();
+    }, 1000);
   });
 })(jQuery);
