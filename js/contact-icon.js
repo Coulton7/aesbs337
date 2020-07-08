8c77e1cc900201bfc039ac3edaf9a7696ae7f2b3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-07-08 14:25:33 +0100

    went wrong and hid both needs adjusting for now reset

--- js/contact-icon.js
+++ js/contact-icon.js
@@ -4,15 +4,20 @@
 
     var contact_icon = jQuery(".contact_icon");
     var contactIcon = -1;
+    var runCount = 0;
 
     function showNextIcon() {
+      if (runCount < 30) {
         ++contactIcon;
         contact_icon.eq(contactIcon % contact_icon.length)
           .show(0)
           .delay(1800)
           .hide(0, showNextIcon);
         runCount++;
-    }
+          } else {
+            contact_icon.eq(contactIcon % contact_icon.length)
+              .show(0);
+        }
     showNextIcon();
   };
 })(jQuery);
