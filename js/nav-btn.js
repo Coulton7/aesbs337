27a6e3ef503c4fde7edc73047fdbf0a168925f01
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-08-21 11:56:10 +0100

    Adding new navigation control for the sustainability page

--- js/nav-btn.js
+++ js/nav-btn.js
@@ -47,4 +47,18 @@
         }
     });
   });
+
+  $(document).ready(function() {
+    $('#prev').on('click', function() {
+      $('.sustain-nav').animate({
+        scrollLeft: '-=200'
+      }, 300, 'swing');
+    });
+
+    $('#next').on('click', function() {
+      $('.sustain-nav').animate({
+        scrollLeft: '+=200'
+      }, 300, 'swing');
+    });
+  });
 })(jQuery);
