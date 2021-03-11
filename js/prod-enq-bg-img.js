950cee307f95fa15438ef78c1044dd96be60f887
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-03-11 16:21:06 +0000

    Updating the js for flexslider variation

--- js/prod-enq-bg-img.js
+++ js/prod-enq-bg-img.js
@@ -1,6 +1,6 @@
 (function($){
   $(document).ready(function($){
-    var imgSrc = $ ('.field-product-image .flexslider .slides li:nth-child(2) img').attr('src');
+    var imgSrc = $ ('.field-product-image .flexslider .slides li:nth-child(1) img').attr('src');
 
     $('.block-productenquiry').css('background-image', 'url(' + imgSrc + ')');
   });
