9d981e66f0d95cd0798d763eb076ced3cd1da180
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-04-01 16:36:09 +0100

    Adding new js for the svg map

---
+++ js/svg-map-ckick.js
@@ -0,0 +1,30 @@
+(function($){
+  $(document).ready(function() {
+    var allRegions = $("#Map > *");
+
+    allRegions.on("click", function() {
+      var link = ($(this).attr('id'));
+      if (link == "Yorkshire_and_the_Humber"){
+        $('.millClose').addClass('show');
+      }
+      allRegions.removeClass("on");
+      $(this).addClass("on");
+      $('body').addClass("noscroll")
+      $('.location-pop-up').dialog({
+        width: "70%",
+        height: 'auto',
+        modal:true,
+        title: 'Location Details',
+        close: function(){
+        },
+      });
+      $('.location-pop-up').on("dialogclose", function(){
+        allRegions.removeClass("on");
+        $('body').removeClass("noscroll");
+        if(link == "Yorkshire_and_the_Humber"){
+          $('.millClose').removeClass('show');
+        }
+      });
+    });
+  });
+})(jQuery);
