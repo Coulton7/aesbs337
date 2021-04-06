b659042954fead3d0100b312328f81a7dcb273bb
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-04-06 11:19:58 +0100

    US states added to script for office known regions added

--- js/svg-map-ckick.js
+++ js/svg-map-ckick.js
@@ -1,15 +1,25 @@
 (function($){
   $(document).ready(function() {
-    var allRegions = $("#Map > *");
+    var allRegions = $("#Land > *");
 
     allRegions.on("click", function() {
       var link = ($(this).attr('id'));
-      if (link == "Yorkshire_and_the_Humber"){
-        $('.millClose').addClass('show');
+
+      if (link == "Land-Tennesse"){
+        $('.rockford').addClass('show');
+      } else if (link == "Land-Maine" || "Land-NH" || "Land-Vt" || "Land-NewJersey" || "Land-NewYork" || "Land-WestVirginia" || "Land-Wisconsin" || "Land-Minnesota") {
+        $('.fairfield').addClass('show');
+      } else if (link == "Land-Washington" || "Land-Oregona" || "Land-Utah" || "Land-Colorado" || "Land-Wyoming" || "Lnad-Montana") {
+        $('.longview').addClass('show');
+      } else if (link == "Land-Iowa" || "Land-Nebraska" || "Land-NorthDakota" || "Land-SouthDakota") {
+        $('.marion').addClass('show');
+      } else if(link == "Land-Texas") {
+        $('.llp' && '.mss').addClass('show');
       }
+
       allRegions.removeClass("on");
       $(this).addClass("on");
-      $('body').addClass("noscroll")
+      $('body').addClass("noscroll");
       $('.location-pop-up').dialog({
         width: "70%",
         height: 'auto',
@@ -21,9 +31,19 @@
       $('.location-pop-up').on("dialogclose", function(){
         allRegions.removeClass("on");
         $('body').removeClass("noscroll");
-        if(link == "Yorkshire_and_the_Humber"){
-          $('.millClose').removeClass('show');
+
+        if(link == "Land-Tennessee"){
+          $('.rockford').removeClass('show');
+        } else if (link == "Land-Maine" || "Land-NH" || "Land-Vt" || "Land-NewJersey" || "Land-NewYork" || "Land-WestVirginia" || "Land-Wisconsin" || "Land-Minnesota") {
+          $('.fairfield').removeClass('show');
+        } else if (link == "Land-Washington" || "Land-Oregona" || "Land-Utah" || "Land-Colorado" || "Land-Wyoming" || "Lnad-Montana") {
+          $('.longview').removeClass('show');
+        } else if (link == "Land-Iowa" || "Land-Nebraska" || "Land-NorthDakota" || "Land-SouthDakota") {
+          $('.marion').removeClass('show');
+        } else if(link == "Land-Texas") {
+          $('.llp' && '.mss').removeClass('show');
         }
+
       });
     });
   });
