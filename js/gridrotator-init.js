b8739726152b592cd02d37ff42f54c1f248a06e0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-27 15:15:15 +0100

    New script for the gridrotaotr, should work like flexlsider does

---
+++ js/gridrotator-init.js
@@ -0,0 +1,64 @@
+(function($) {
+
+    $(document).ready(function() {
+      $('#ri-grid').gridrotator({
+        rows: 3,
+        columns: 4,
+        animType: 'slideBottom',
+        animSpeed: 1000,
+        interval: 600,
+        step: 1,
+        interval: 2000,
+        w1024: {
+          rows: 3,
+          columns: 4
+        },
+        w768: {
+          rows: 4,
+          columns: 3
+        },
+        w480: {
+          rows: 4,
+          columns: 3
+        },
+        w320: {
+          rows: 2,
+          columns: 4
+        },
+        w240: {
+          rows: 1,
+          columns: 7
+        },
+      });
+    });
+
+    $('#six-grid').gridrotator({
+      rows: 2,
+      columns: 6,
+      animType: 'slideBottom',
+      animSpeed: 1000,
+      interval: 600,
+      step: 1,
+      interval: 2000,
+      w992: {
+        rows: 2,
+        columns: 6
+      },
+      w768: {
+        rows: 4,
+        columns: 3
+      },
+      w480: {
+        rows: 3,
+        columns: 4
+      },
+      w320: {
+        rows: 3,
+        columns: 4
+      },
+      w240: {
+        rows: 1,
+        columns: 7
+      },
+    });
+});
