2591707d53e6cd368055f279963006a956a084d6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-06-01 15:14:03 +0100

    Adding new carousel options js file

---
+++ js/carousel-options.js
@@ -0,0 +1,43 @@
+(function($) {
+  //Function to animate slider captions
+  function doAnimations(elems) {
+    var animEndEv = "webkitAnimationEnd animationend";
+
+    elems.each(function() {
+      var $this = $(this),
+        $animationType = $this.data("animation");
+      $this.addClass($animationType).one(animEndEv, function() {
+        $this.removeClass($animationType);
+      });
+    });
+  }
+
+  var $myCarousel = $("#carousel-fade"),
+    $firstAnimatingElems = $myCarousel
+    .find(".carousel-item:first")
+    .find("[data-animation ^= 'animated']");
+
+  $myCarousel.carousel();
+
+  doAnimations($firstAnimatingElems);
+
+  $myCarousel.on("slide.bs.carousel", function(e) {
+    var $animatingElems = $(e.relatedTarget).find(
+      "[data-animation ^= 'animated']"
+    );
+    doAnimations($animatingElems);
+  });
+
+  $('#pauseButton').click(function(){
+    $myCarousel.carousel('pause');
+    $('#pauseButton').addClass('display-none');
+    $('#playButton').removeClass('display-none;');
+  });
+
+  $('#playButton').click(function(){
+    $myCarousel.carousel('cycle');
+    $('#pauseButton').removeClass('display-none');
+    $('#playButton').addClass('display-none;');
+  });
+
+})(jQuery);
