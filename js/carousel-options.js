aa1abc623a3625bc6ef27551f131b0782ed187b6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-06-01 16:50:17 +0100

    Adding a js to set the height of the slides onload

--- js/carousel-options.js
+++ js/carousel-options.js
@@ -40,4 +40,35 @@
     $('#playButton').addClass('display-none;');
   });
 
+  function carouselNormalization() {
+  var items = $('#carousel-fade .item'),
+    heights = [],
+    tallest;
+
+  if (items.length) {
+    function normalizeHeights() {
+      items.each(function() {
+        heights.push($(this).height());
+      });
+      tallest = Math.max.apply(null, heights);
+      items.each(function() {
+        $(this).css('min-height', tallest + 'px');
+      });
+    }
+    normalizeHeights();
+
+    $(window).on('resize orientationchange', function() {
+      tallest = 0, heights.length = 0;
+      items.each(function() {
+        $(this).css('min-height', '0');
+      });
+      normalizeHeights();
+    });
+  }
+}
+
+window.onload = function() {
+  carouselNormalization();
+};
+
 })(jQuery);
