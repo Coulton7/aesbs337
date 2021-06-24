e8ba8e8ce9ffa16207ea74532c63ffcbf0b2b1fa
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-06-24 12:15:32 +0100

    New js for the newsCarousel

--- js/carousel-options.js
+++ js/carousel-options.js
@@ -113,7 +113,24 @@ $(document).ready(function() {
       if (!next.length) {
       	next = $(this).siblings(':first');
     	}
-      
+
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
+
+  $('.carousel .horizontal .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=1;i<3;i++) {
+      next=next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+
       next.children(':first-child').clone().appendTo($(this));
     }
   });
