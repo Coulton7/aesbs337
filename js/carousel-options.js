7273efac45901ef9bfa72130682532904f490aaf
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-06-25 11:28:41 +0100

    Replaced back to prev just need to figure out solution for 1st 2 slides

--- js/carousel-options.js
+++ js/carousel-options.js
@@ -119,31 +119,31 @@ $(document).ready(function() {
   });
 
   $('.carousel .horizontal .item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
+    var prev = $(this).prev();
+    if (!prev.length) {
+      prev = $(this).siblings(':first');
     }
-    next.children(':first-child').clone().appendTo($(this));
+    prev.children(':first-child').clone().appendTo($(this));
 
     if (jQuery(window).width() > 992) {
       for (var i = 1; i < 2; i++) {
-        next = next.next();
-        if (!next.length) {
-          next = $(this).siblings(':first');
+        prev = prev.prev();
+        if (!prev.length) {
+          prev = $(this).siblings(':first');
         }
 
-        next.children(':first-child').clone().appendTo($(this));
+        prev.children(':first-child').clone().appendTo($(this));
       }
     }
 
     else if (jQuery(window).width() > 768) {
       for (var x = 1; x < 1; x++) {
-        next = next.next();
-        if (!next.length) {
-          next = $(this).siblings(':first');
+        prev = prev.prev();
+        if (!prev.length) {
+          prev = $(this).siblings(':first');
         }
 
-        next.children(':first-child').clone().appendTo($(this));
+        prev.children(':first-child').clone().appendTo($(this));
       }
     }
 
