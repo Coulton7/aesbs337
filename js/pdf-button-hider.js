5a51ef7390cb3b3cd874fc664ffa008e691e8104
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-07-02 15:22:05 +0100

    Adding an animate function to the script to see if it'll scroll up to it

--- js/pdf-button-hider.js
+++ js/pdf-button-hider.js
@@ -6,6 +6,9 @@
        if (orgName.value == "") {
          $(orgName).addClass("is-invalid");
          $('.invalid-feedback').removeClass('display-none');
+         $('html, body').animate({
+           scrollTop: ($('.is-invalid').offset().top)
+         }, 1000);
          return false;
 
        } else {
