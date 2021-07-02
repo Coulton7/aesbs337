956c9bf2c4c19fa622f3d9a15b9a7cfedf0b3d85
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-07-02 13:33:39 +0100

    New attempt at prefilling text values

--- js/pdfcreator.js
+++ js/pdfcreator.js
@@ -2,12 +2,48 @@
   function pdfCreator() {
     var orgName = document.getElementById('orgName');
     var orgPhone = document.getElementById('orgPhone');
+    var phone;
+    if(orgPhone === ""){
+      phone = "01632 123456";
+    } else {
+      phone = orgPhone.value;
+    }
     var orgEmail = document.getElementById('orgEmail');
+    var email;
+    if(orgEmail === ""){
+      email = "example@domain.com";
+    } else {
+      email = orgEmail.value;
+    }
     var orgWorkers = document.getElementById('orgWorkers');
+    var workers;
+    if(orgWorkers === ""){
+      workers = "example@domain.com";
+    } else {
+      workers = orgWorkers.value;
+    }
     var roi = document.getElementById('roi');
+    var roiTime;
+    if(roi === ""){
+      roiTime = "4 Years";
+    } else {
+      roiTime = roi.value;
+    }
     var orgType = document.getElementById('orgType').value;
     var jobRole = document.getElementById('jobRole');
+    var role;
+    if(role === ""){
+      role = "Managing Director";
+    } else {
+      role = jobRole.value;
+    }
     var name = document.getElementById('name');
+    var nameValue;
+    if(name === ""){
+      nameValue = "John Smith";
+    } else {
+      nameValue = name.value;
+    }
     var orgTypeValue;
     if (document.getElementById('type1').checked) {
       orgTypeValue = document.getElementById('type1').value;
@@ -15,14 +51,14 @@
       orgTypeValue = document.getElementById('type2').value;
     } else if (document.getElementById('type3').checked) {
       orgTypeValue = document.getElementById('type3').value;
-    };
+    }
     var date = document.getElementById('date');
     var startDate;
     if (date.value === "") {
-      startDate = new Intl.DateTimeFormat('en-GB').format()
+      startDate = new Intl.DateTimeFormat('en-GB').format();
     } else {
       startDate = date.value;
-    };
+    }
 
 
     var docDefinition = {
@@ -64,20 +100,20 @@
           ul: [
             'Any sustainability project will be given priority over any other capital investment with a similar Return on Investment.\n\n',
             {
-              text: ['Any sustainability project with a reasonable chance of getting a Return on Investment of ', roi.value, ' or less, should be brought to the attention of the ', orgName.value, '.\n\n']
+              text: ['Any sustainability project with a reasonable chance of getting a Return on Investment of ', roiTime, ' or less, should be brought to the attention of the ', orgName.value, '.\n\n']
             },
             'A written decision on any such project is mandatory within 3 months of project submission.\n\n',
             {
               text: ['All global business heads have local spending authority. Any sustainability project with a higher value should be immediately referred to the Board of ', orgName.value, '.\n\n']
             },
             {
-              text: ['As a Board we undertake to use our personal and corporate presence to influence policy makers to legislate the requirement for such a policy for all businesses with more than ', orgWorkers.value, ' co-workers.\n\n']
+              text: ['As a Board we undertake to use our personal and corporate presence to influence policy makers to legislate the requirement for such a policy for all businesses with more than ', workers, ' co-workers.\n\n']
             },
             'Current or potential supplier input is welcomed, including their use of the global hotline where necessary.\n\n'
           ],
         },
         {
-          text: ['The Board of ', orgName.value, ' encourage employees and stakeholders to contact us by ', orgPhone.value, ' or by email on ', orgEmail.value, ' to assist with the discreet implementation of this policy where necessary.']
+          text: ['The Board of ', orgName.value, ' encourage employees and stakeholders to contact us by ', phone, ' or by email on ', email, ' to assist with the discreet implementation of this policy where necessary.']
         },
         {
           text: ['\n\n']
@@ -86,10 +122,10 @@
           text: ['.......................................................................................\n\n']
         },
         {
-          text: ['', name.value, '\n\n']
+          text: ['', nameValue, '\n\n']
         },
         {
-          text: ['', jobRole.value, '']
+          text: ['', role, '']
         },
         {
           text: ['', startDate],
@@ -130,12 +166,48 @@
   function pdfOpen() {
     var orgName = document.getElementById('orgName');
     var orgPhone = document.getElementById('orgPhone');
+    var phone;
+    if(orgPhone === ""){
+      phone = "01632 123456";
+    } else {
+      phone = orgPhone.value;
+    }
     var orgEmail = document.getElementById('orgEmail');
+    var email;
+    if(orgEmail === ""){
+      email = "example@domain.com";
+    } else {
+      email = orgEmail.value;
+    }
     var orgWorkers = document.getElementById('orgWorkers');
+    var workers;
+    if(orgWorkers === ""){
+      workers = "example@domain.com";
+    } else {
+      workers = orgWorkers.value;
+    }
     var roi = document.getElementById('roi');
+    var roiTime;
+    if(roi === ""){
+      roiTime = "4 Years";
+    } else {
+      roiTime = roi.value;
+    }
     var orgType = document.getElementById('orgType').value;
     var jobRole = document.getElementById('jobRole');
+    var role;
+    if(role === ""){
+      role = "Managing Director";
+    } else {
+      role = jobRole.value;
+    }
     var name = document.getElementById('name');
+    var nameValue;
+    if(name === ""){
+      nameValue = "John Smith";
+    } else {
+      nameValue = name.value;
+    }
     var orgTypeValue;
     if (document.getElementById('type1').checked) {
       orgTypeValue = document.getElementById('type1').value;
@@ -143,14 +215,14 @@
       orgTypeValue = document.getElementById('type2').value;
     } else if (document.getElementById('type3').checked) {
       orgTypeValue = document.getElementById('type3').value;
-    };
+    }
     var date = document.getElementById('date');
     var startDate;
     if (date.value === "") {
-      startDate = new Intl.DateTimeFormat('en-GB').format()
+      startDate = new Intl.DateTimeFormat('en-GB').format();
     } else {
       startDate = date.value;
-    };
+    }
 
 
     var docDefinition = {
@@ -163,7 +235,7 @@
       pageMargins: [45, 105, 45, 60],
 
       footer: {
-        margin: [35, 0, 45, 0],
+        margin: [45, 0, 45, 0],
         text: [
           'This policy is copyright free and any, or all of it can be adopted at will by any organisation. Create your own by visiting, https://www.aesseal.com/en/resources/industry-guides/policy-prevent-global-warming'
         ],
@@ -192,20 +264,20 @@
           ul: [
             'Any sustainability project will be given priority over any other capital investment with a similar Return on Investment.\n\n',
             {
-              text: ['Any sustainability project with a reasonable chance of getting a Return on Investment of ', roi.value, ' or less, should be brought to the attention of the ', orgName.value, '.\n\n']
+              text: ['Any sustainability project with a reasonable chance of getting a Return on Investment of ', roiTime, ' or less, should be brought to the attention of the ', orgName.value, '.\n\n']
             },
             'A written decision on any such project is mandatory within 3 months of project submission.\n\n',
             {
               text: ['All global business heads have local spending authority. Any sustainability project with a higher value should be immediately referred to the Board of ', orgName.value, '.\n\n']
             },
             {
-              text: ['As a Board we undertake to use our personal and corporate presence to influence policy makers to legislate the requirement for such a policy for all businesses with more than ', orgWorkers.value, ' co-workers.\n\n']
+              text: ['As a Board we undertake to use our personal and corporate presence to influence policy makers to legislate the requirement for such a policy for all businesses with more than ', workers, ' co-workers.\n\n']
             },
             'Current or potential supplier input is welcomed, including their use of the global hotline where necessary.\n\n'
           ],
         },
         {
-          text: ['The Board of ', orgName.value, ' encourage employees and stakeholders to contact us by ', orgPhone.value, ' or by email on ', orgEmail.value, ' to assist with the discreet implementation of this policy where necessary.\n\n']
+          text: ['The Board of ', orgName.value, ' encourage employees and stakeholders to contact us by ', phone, ' or by email on ', email, ' to assist with the discreet implementation of this policy where necessary.']
         },
         {
           text: ['\n\n']
@@ -214,10 +286,10 @@
           text: ['.......................................................................................\n\n']
         },
         {
-          text: ['', name.value, '']
+          text: ['', nameValue, '\n\n']
         },
         {
-          text: ['', jobRole.value, '']
+          text: ['', role, '']
         },
         {
           text: ['', startDate],
