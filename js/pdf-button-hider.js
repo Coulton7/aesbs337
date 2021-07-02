c76a37166f09fa8851ef5b98729e78ecce764408
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-07-02 14:17:15 +0100

    Adding js to show error properly

--- js/pdf-button-hider.js
+++ js/pdf-button-hider.js
@@ -5,6 +5,7 @@
 
        if (orgName.value == "") {
          $(orgName).addClass("is-invalid");
+         $('.invalid-feedback').removeClass('display-none');
          return false;
 
        } else {
