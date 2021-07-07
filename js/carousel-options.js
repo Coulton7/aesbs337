159960e7eead80c18300e5532ba4d13092fe6cc2
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-07-07 15:16:15 +0100

    Re-added & adjusted resize script for the homepage + jquery changed to $

--- js/carousel-options.js
+++ js/carousel-options.js
@@ -90,7 +90,7 @@
       }
       next.children(':first-child').clone().appendTo($(this));
 
-      if (jQuery(window).width() > 992) {
+      if ($(window).width() > 992) {
         for (var i = 1; i < 2; i++) {
           next = next.prev();
           if (!next.length) {
@@ -99,7 +99,7 @@
 
           next.children(':first-child').clone().appendTo($(this));
         }
-      } else if (jQuery(window).width() > 768) {
+      } else if ($(window).width() > 768) {
         for (var x = 1; x < 1; x++) {
           next = next.prev();
           if (!next.length) {
@@ -108,12 +108,45 @@
 
           next.children(':first-child').clone().appendTo($(this));
         }
-      } else if (jQuery(window).width() < 767) {
+      } else if ($(window).width() < 767) {
 
       }
 
     });
 
+  });
+
+  $(window).on('resize orientationchange', function(){
+    $('.carousel .horizontal .item').each(function() {
+      var next = $(this).prev();
+      if (!next.length) {
+        next = $(this).siblings(':last');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+
+      if ($(window).width() > 992) {
+        for (var i = 1; i < 2; i++) {
+          next = next.prev();
+          if (!next.length) {
+            next = $(this).siblings(':last');
+          }
+
+          next.children(':first-child').clone().appendTo($(this));
+        }
+      } else if ($(window).width() > 768) {
+        for (var x = 1; x < 1; x++) {
+          next = next.prev();
+          if (!next.length) {
+            next = $(this).siblings(':last');
+          }
+
+          next.children(':first-child').clone().appendTo($(this));
+        }
+      } else if ($(window).width() < 767) {
+
+      }
+
+    });
   });
 
 })(jQuery);
