578e000e11eee482b68ddfb9c88e250b9440e9c3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-07-19 09:38:53 +0100

    New tab heihgt based script to control tab-pannel sizes

--- js/tabs-hash-nav.js
+++ js/tabs-hash-nav.js
@@ -6,4 +6,15 @@
       $(hashTab).addClass('active');
     }
   });
+
+  $('#media-tabs .media-tab').hide();
+  var tabDivHeight = 0;
+  $('#media-tabs .media-tab').each(function () {
+    $(this).show();
+    if ($(this).height() > tabDivHeight)
+      tabDivHeight = $(this).height();
+    $(this).hide();
+  });
+  $('#media-tabs .media-tab').height(tabDivHeight);
+
 })(jQuery);
