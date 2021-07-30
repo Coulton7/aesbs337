bc4d397bc387b8894d3693012c75a655aa51ed35
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-07-30 14:10:59 +0100

    Trying with an adjustment to the result tpl to show content type

--- templates/search-api-page-result.tpl.php
+++ templates/search-api-page-result.tpl.php
@@ -92,7 +92,7 @@
 		<?php print $url ? l($title, $url['path'], $url['options']) : check_plain($title); ?>
 		</h3>
     <h6>
-      <?php print $info_split['type']; ?>
+      <?php print $result['type']; ?>
     </h6>
 		<div class="search-snippet-info">
 				<?php if ($snippet): ?>
