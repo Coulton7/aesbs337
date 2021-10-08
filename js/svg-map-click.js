231614cf0813a1b8f97a8eb3aa1b579e7aab21f7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-10-08 10:15:11 +0100

    Trying a new position command on the js for testing

--- js/svg-map-click.js
+++ js/svg-map-click.js
@@ -385,6 +385,7 @@
         width: "70%",
         height: ($(window).height()-250),
         modal: true,
+        position: {my:"bottom", at:"bottom", of: screen},
         title: "Regional sales & contact Details for "+ state,
         draggable: false,
         resizable: false,
