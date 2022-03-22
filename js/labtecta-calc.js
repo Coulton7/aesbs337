502b898faa2f90bff4062b7cba861dd93e50093e
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-03-22 14:40:48 +0000

    New js improvements for mobile to get it working

--- js/labtecta-calc.js
+++ js/labtecta-calc.js
@@ -1,6 +1,13 @@
 (function($) {
+
+  function getPumpId() {
+    return (window.innerWidth < 768) ? document.getElementById('mobPumps').value:
+           (window.innerWidth > 768) ? document.getElementById('pumps').value: document.getElementById('pumps').value;
+  }
+
+
   function calculate() {
-    var pumps = document.getElementById('pumps').value,
+    var pumps = getPumpId(),
         cost = 294.06,
         costYear = pumps * cost,
         fiveYear = costYear * 5,
@@ -11,11 +18,24 @@
       document.getElementById('annualYear').innerHTML = costYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
       document.getElementById('powerConsumption').innerHTML = powerConY.toLocaleString("en-GB");
       document.getElementById('fiveYear').innerHTML = fiveYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
+
+      document.getElementById('mobLipSealCost').innerHTML = costYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
+      document.getElementById('mobAnnualYear').innerHTML = costYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
+      document.getElementById('mobPowerConsumption').innerHTML = powerConY.toLocaleString("en-GB");
+      document.getElementById('mobFiveYear').innerHTML = fiveYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
   }
 
   $(document).ready(function() {
     document.getElementById('calculate').addEventListener("click", function() {
       calculate();
     });
+
+    document.getElementById('mobcalculate').addEventListener("touchstart", function() {
+      calculate();
+    });
   });
+
+  $(window).resize(function(){
+    getPumpId();
+  })
 })(jQuery);
