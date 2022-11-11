6770aca980256257d2d641ed815e2085901f9433
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-11-11 11:33:13 +0000

    Cleaned p the format of the js

--- js/nav-btn.js
+++ js/nav-btn.js
@@ -14,22 +14,22 @@
             $('.navigation_col').removeClass('overlay-menu');
             $(y).addClass('fixed-top');
           } else {
-              x.style.height = "100%";
-              if($('#prodForm').hasClass('in')) {
-                $('#prodForm').modal('hide')
-              }
-              if($('#resForm').hasClass('in')) {
-                $('#resForm').modal('hide')
-              }
-              if($('#location-details').dialog('isOpen') === true) {
-                $('#location-details').dialog('close');
-              }
-              $('.main').addClass('active');
-              $("body").addClass('noscroll');
-              $('#overlay-btn').removeClass('overlay-btn');
-              $('.search-block').addClass('overlay-search');
-              $('.navigation_col').addClass('overlay-menu');
-              $(y).removeClass('fixed-top');
+            x.style.height = "100%";
+            if($('#prodForm').hasClass('in')) {
+              $('#prodForm').modal('hide')
+            }
+            if($('#resForm').hasClass('in')) {
+              $('#resForm').modal('hide')
+            }
+            if($('#location-details').dialog('isOpen') === true) {
+              $('#location-details').dialog('close');
+            }
+            $('.main').addClass('active');
+            $("body").addClass('noscroll');
+            $('#overlay-btn').removeClass('overlay-btn');
+            $('.search-block').addClass('overlay-search');
+            $('.navigation_col').addClass('overlay-menu');
+            $(y).removeClass('fixed-top');
           }
       });
 
